cd848b42181cdedf3d260e6ad235af3fa4a57080
Gcrouzet <58287057+Gcrouzet@users.noreply.github.com>
2020-05-25 13:44:43 +0200

    debut exercice employee

--- CDA/src/FOAD_ALGO/Employees.js
+++ CDA/src/FOAD_ALGO/Employees.js
@@ -37,20 +37,43 @@
 /**
  * @class Employee
  */
-class Employee
-{
-    constructor(_id, _lastname, _firstname, _role, _salary, _hiredate ){
-        this.id=_id;
-        this.lastname=_lastname;
-        this.firstname=_firstname;
-        this.email= _firstname.substring(0, 1).toLowerCase() + _lastname.toLowerCase()+"@email.fr";
-        this.role=_role;
-        this.salary=_salary;
-        this.hiredate=_hiredate;
+class Employee {
+    constructor(_id, _lastname, _firstname, _role, _salary, _hiredate) {
+        this.id = _id;
+        this.lastname = _lastname;
+        this.firstname = _firstname;
+        this.email = _firstname.substring(0, 1).toLowerCase() + _lastname.toLowerCase() + "@email.fr";
+        this.role = _role;
+        this.salary = _salary;
+        this.hiredate = _hiredate.toISOString().substring(0, 10);
     }
 
-}
+    getMonthlySalary = function () {
+        return Math.round(this.salary / 12 * 0.75);
+    }
+    getSeniority = function () {
+        let now = new Date();
+        let embaucheDate = new Date(this.hiredate.replace('-', ','));
+        let diff = now.getTime() - embaucheDate.getTime();
+        diff = Math.floor(diff / (1000 * 60 * 60 * 24));
+        let année = Math.floor(diff / 365);
+        diff = diff - année * 365;
+        let mois = Math.floor(diff / 30);
+        diff = diff - mois * 30;
+        let jour = diff;
 
+        if (now < embaucheDate) {
+            return "L'employé n'est pas encore dans l'entreprise";
+        }
+        if (jour > 0 && mois > 0 && année > 0) {
+            return année + " ans et " + mois + " mois et " + jour + " jours ";
+        } else if (jour > 0 && mois > 0 && année == 0) {
+            return mois + " mois et " + jour + " jours ";
+        } else if (jour > 0 && mois == 0 && année == 0) {
+            return jour + " jours ";
+        }
+    }
+}
 
 
 /** DÉBUT ZONE NON EDITABLE (Ne pas modifier les lignes suivantes) */
@@ -58,9 +81,14 @@ class Employee
 
 /** @var Employee employee1 */
 var employee1 = new Employee(1, 'Doe', 'John', 'manager', 82000, new Date('2020-12-28')); // création d'un nouvel employé
+var employee2 = new Employee(2, 'Crouzet', 'Gabriel', 'Boss', 500000, new Date('2010-10-21'));
+var employee3 = new Employee(3, 'Torrenti', 'Sylvain', 'Expert (partage)', 150000, new Date('2018-02-20'));
+var employee4 = new Employee(4, 'Schmitt', 'Joanna', 'Directrice', 250000, new Date('2015-08-18'));
+var employee5 = new Employee(5, 'Hamza', 'Reda', 'Vendeur sénior', 50000, new Date('1950-05-10'));
 
 /** @var array employees */
 const employees = [employee1]; // tableau contenant les employés
+employees.push(employee2, employee3, employee4, employee5);
 
 console.log(employee1); // doit afficher l'objet "employee1"
 console.log("Il y a " + employees.length + " employé(e)s."); // doit afficher "Il y a 5 employé(e)s."
@@ -69,6 +97,13 @@ console.log(employees); // export des employés dans la console
 
 /** FIN ZONE NON EDITABLE (Ne pas modifier les lignes précédentes) */
 
+for (let i = 0; i < employees.length; i++) {
+    console.log(i + 1 + ' - ' + employees[i].lastname +
+        ' ' + employees[i].firstname +
+        ', ' + employees[i].email +
+        ', ' + employees[i].getSeniority() +
+        ', ' + employees[i].getMonthlySalary() + '€');
+}
 
 
-// Écrivez votre code à partir de la ligne suivante...
+

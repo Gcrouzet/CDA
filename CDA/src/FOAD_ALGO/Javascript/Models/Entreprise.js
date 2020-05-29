c7c047c3f8bec4350cd4f58373afe1f1c10fe45f
Gcrouzet <58287057+Gcrouzet@users.noreply.github.com>
2020-05-29 11:30:31 +0200

    class Entreprise

--- CDA/src/FOAD_ALGO/Javascript/Models/Entreprise.js
+++ CDA/src/FOAD_ALGO/Javascript/Models/Entreprise.js
@@ -3,8 +3,7 @@ const Employee = require('./Employee.js');
 /**
  * Gestion d'employés
  */
-class Enterprise 
-{
+class Enterprise {
     constructor() {
         this.employees = [];
     }
@@ -14,7 +13,7 @@ class Enterprise
      * @param  _filter 
      */
     readAll(_filter) {
-        
+
     }
 
     /**
@@ -22,57 +21,86 @@ class Enterprise
      * @param Employee _employee 
      */
     create(_employee) {
-
+        this.employees.push(_employee);
     }
 
     /**
      * 
      * @param int _id 
      */
-    read(_id) { 
-
+    read(_id) {
+        for (let i = 0; i < this.employees.length; i++) {
+            if (this.employees[i].id == _id) {
+                return this.employees[i];
+                break;
+            }
+        }
     }
 
     /**
      * Met à jour un employé
      * @param Employee _employee 
      */
-    update(_employee) {
-
+    update(_employee, _newRole) {
+        _employee.role = _newRole;
     }
-    
+
     /**
      * Supprime un employé
      * @param int _id 
      */
     delete(_id) {
+        for (let i = 0; i < this.employees.length; i++) {
+            if (this.employees[i].id == _id) {
+                this.employees.splice(i, 1);
+                break;
+
+            }
+        }
 
     }
 
+
+
+    /**
+     * Augmentation du salaire
+     */
+    getHigherSalary(_employee, _newSalary) {
+        if (_newSalary > _employee.salary) {
+            _employee.salary = _newSalary;
+        }
+    }
+
+    /**
+     * Diminution du salaire
+     */
+    getLowerSalary(_employee, _newSalary) {
+        if (_newSalary < _employee.salary) {
+            _employee.salary = _newSalary;
+        }
+    }
 
     /**
      * 
      */
-    getHigherSalary() {
-
-    }
-
-    /**
-     * 
-     */
-    getLowerSalary() {
-
-    }
-
-    /**
-     * 
-     */
-    getSalaryGap() {
-
+    getSalaryGap(_employeeA, _employeeB) {
+        let diff;
+        if (_employeeA.salary == _employeeB.salary) {
+            return "Ces deux employés perçoivent le même salaire";
+        }
+        if (_employeeA.salary > _employeeB.salary) {
+            diff = _employeeA.salary - _employeeB.salary;
+            return _employeeA.firstname + " perçoit " + diff + "€/an de plus que " + _employeeB.firstname;
+        }
+        if (_employeeB.salary > _employeeA.salary) {
+            diff = _employeeB.salary - _employeeA.salary;
+            return _employeeB.firstname + " perçoit " + diff + "€/an de plus que " + _employeeA.firstname;
+        }
     }
 
 
-    
+
+
 
 
 
@@ -80,3 +108,4 @@ class Enterprise
 
 
 module.exports = Enterprise;
+

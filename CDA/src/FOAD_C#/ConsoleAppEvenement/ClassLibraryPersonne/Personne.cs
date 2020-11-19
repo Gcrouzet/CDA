aff184a9053cf129c9139f6eda723580047466f0
GCrouzet <ga.crouzet@hotmail.fr>
2020-11-19 10:06:10 +0100

    crise Personne

---
+++ CDA/src/FOAD_C#/ConsoleAppEvenement/ClassLibraryPersonne/Personne.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryPersonne
+{
+    public class Personne
+    {
+        private string name;
+        private int age;
+
+        public string Name { get => name; /*set => name = value;*/ }
+        public int Age { get => age; /*set => age = value;*/ }
+
+        public delegate void DelegateMajeur(Personne sender);
+        public event DelegateMajeur JeViensDEtreMajeur;
+
+        //rajouter un evenement RisqueCrise...
+        public delegate void DelegateRisqueCrise(Personne sender, string typeDeCrise);
+        public event DelegateRisqueCrise JeRisqueUneCrise;
+
+
+        public Personne(string name)
+        {
+            this.name = name;
+            age = 0;
+        }
+
+        public void VieillirDUnAn()
+        {
+            age++;
+            if (age == 18)
+            {
+                if (JeViensDEtreMajeur != null) //verification que qq s'est enregistre
+                {
+                    JeViensDEtreMajeur(this);
+                }
+            }
+
+            if (JeRisqueUneCrise != null)
+            {
+                switch (this.age)
+                {
+                    case 15:
+                        JeRisqueUneCrise(this, "la crise d'adolescence");
+                        break;
+                    case 30:
+                        JeRisqueUneCrise(this, "la crise de la trentaine");
+                        break;
+                    case 40:
+                        JeRisqueUneCrise(this, "la crise de la quarantaine");
+                        break;
+                    case 50:
+                        JeRisqueUneCrise(this, "la crise de la cinquantaine");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ CDA/src/FOAD_C#/ConsoleAppEvenement/ConsoleAppEvenement/Program.cs
@@ -0,0 +1,42 @@
+﻿using ClassLibraryPersonne;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppEvenement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personne p = new Personne("Robert");
+            Personne p2 = new Personne("Berthe");
+
+            p.JeViensDEtreMajeur += P_JeViensDEtreMajeur;
+            p2.JeViensDEtreMajeur += P_JeViensDEtreMajeur;
+
+            p.JeRisqueUneCrise += P_JeRisqueUneCrise;
+            p2.JeRisqueUneCrise += P_JeRisqueUneCrise;
+
+            for (int i = 0; i < 100; i++)
+            {
+                p.VieillirDUnAn();
+                p2.VieillirDUnAn();
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void P_JeRisqueUneCrise(Personne sender, string typeDeCrise)
+        {
+            Console.WriteLine(sender.Name + " risque " + typeDeCrise);
+        }
+
+        private static void P_JeViensDEtreMajeur(Personne sender)
+        {
+            Console.WriteLine("Ca y est " + sender.Name + " est majeur! Il peut se debrouiller par lui même");
+        }
+    }
+}
